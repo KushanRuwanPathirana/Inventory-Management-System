6a9aa329f31cf926c6d0f5a0eea9bfdca43f3df4
Kushan Ruwanpathirana <kushanruwan1@gmail.com>
2025-12-23 00:57:04 +0530

    Materials and start gui

---
+++ Dashboard.py
@@ -0,0 +1,11 @@
+from tkinter import *
+class IMS:
+    def __init__(self, root):
+        self.root = root
+        self.root.geometry("1350x700+0+0")
+        self.root.title("Inventory Management System | Developed by Kushan")
+
+
+root=Tk()
+obj=IMS(root)
+root.mainloop() 
