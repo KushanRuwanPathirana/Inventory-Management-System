6be0148a3a88efbb9c79f4b0023daef4929cfd39
Kushan Ruwanpathirana <kushanruwan1@gmail.com>
2025-12-24 03:02:00 +0530

    create gui components

--- Dashboard.py
+++ Dashboard.py
@@ -1,10 +1,67 @@
 from tkinter import *
+from PIL import Image, ImageTk   #PIP INSTALL PILLOW
+
 class IMS:
     def __init__(self, root):
         self.root = root
         self.root.geometry("1350x700+0+0")
         self.root.title("Inventory Management System | Developed by Kushan")
+        self.root.config(bg="white")
+        
+        #===========Title===================
+        img = Image.open("images/logo.png")
+        img = img.resize((120, 70))  # Resize for title bar
+        self.icon_title = ImageTk.PhotoImage(img)
+        
+        #self.icon_title=PhotoImage(file="images/logo.png")
+        title=Label(self.root,text="Inventory Management System",image=self.icon_title,compound=LEFT,font=("times new roman", 40, "bold"),bg="#010c48",fg="white",anchor="w",padx=20).place(x=0,y=0,relwidth=1,height=70)
 
+        #===========Logout Button===================
+        btn_logout=Button(self.root,text="Logout",font=("times new roman", 15, "bold"),bg="yellow",cursor="hand2").place(x=1150,y=10,height=50,width=150)
+        
+        #===========Clock===================
+        self.lbl_clock=Label(self.root,text="Welcome toInventory Management System\t\t Date: DD-MM-YYYY\t\t Time: HH:MM:SS",font=("times new roman", 15),bg="#4d636d",fg="white")
+        self.lbl_clock.place(x=0,y=70,relwidth=1,height=30)
+        
+        #===========Left Menu ===================
+        self.MenuLogo=Image.open("images/menu_im.png")
+        self.MenuLogo=self.MenuLogo.resize((200,200))
+        self.menuLogo=ImageTk.PhotoImage(self.MenuLogo)
+        
+        LeftMenu=Frame(self.root,bd=2,relief=RIDGE,bg="white")
+        LeftMenu.place(x=0,y=100,width=200, height=560)
+        
+        lbl_menuLogo=Label(LeftMenu,image=self.menuLogo)
+        lbl_menuLogo.pack(side=TOP,fill=X)
+        
+        self.icon_side=PhotoImage(file="images/side.png")
+        
+        lbl_menu=Label(LeftMenu,text="Menu",font=("times new roman", 20),bg="#009688").pack(side=TOP,fill=X)
+        btn_employee=Button(LeftMenu,text="Employee",image=self.icon_side, compound=LEFT,anchor="w",font=("times new roman", 20),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+        btn_supplier=Button(LeftMenu,text="Supplier",image=self.icon_side, compound=LEFT,anchor="w",font=("times new roman", 20),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+        btn_category=Button(LeftMenu,text="Category",image=self.icon_side, compound=LEFT,anchor="w",font=("times new roman", 20),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+        btn_product=Button(LeftMenu,text="Product",image=self.icon_side, compound=LEFT,anchor="w",font=("times new roman", 20),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+        btn_sales=Button(LeftMenu,text="Sales",image=self.icon_side, compound=LEFT,anchor="w",font=("times new roman", 20),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+        btn_exit=Button(LeftMenu,text="Exit",image=self.icon_side, compound=LEFT,anchor="w",font=("times new roman", 20),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+        
+        #===========content===================
+        self.lbl_employee=Label(self.root,text="Total Employee\n[ 0 ]",bd=5,relief=RIDGE,font=("times new roman", 20, "bold"),bg="#33bbf9",fg="white")
+        self.lbl_employee.place(x=300,y=120,height=150,width=300)
+
+        self.lbl_supplier=Label(self.root,text="Total Supplier\n[ 0 ]",bd=5,relief=RIDGE,font=("times new roman", 20, "bold"),bg="#ff5722",fg="white")
+        self.lbl_supplier.place(x=650,y=120,height=150,width=300)
+
+        self.lbl_category=Label(self.root,text="Total Category\n[ 0 ]",bd=5,relief=RIDGE,font=("times new roman", 20, "bold"),bg="#009688",fg="white")
+        self.lbl_category.place(x=1000,y=120,height=150,width=300)
+         
+        self.lbl_product=Label(self.root,text="Total Product\n[ 0 ]",bd=5,relief=RIDGE,font=("times new roman", 20, "bold"),bg="#607d8b",fg="white")
+        self.lbl_product.place(x=300,y=300,height=150,width=300)
+
+        self.lbl_sales=Label(self.root,text="Total Sales\n[ 0 ]",bd=5,relief=RIDGE,font=("times new roman", 20, "bold"),bg="#ffc107",fg="white")
+        self.lbl_sales.place(x=650,y=300,height=150,width=300)
+
+        #===========Footer===================
+        lbl_footer=Label(self.root,text="IMS-Inventory Management System | Developed by Kushan \n For any Technical Issue Contact +94 78 4800386",font=("times new roman", 15),bg="#4d636d",fg="white").pack(side=BOTTOM,fill=X)
 
 root=Tk()
 obj=IMS(root)
